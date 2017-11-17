727a17a9c2268dda557a378c197722235c53c517
mesmer <mesmer@fisica.if.uff.br>
2017-11-17 22:06:14 +0000

    audio follow video

--- test3.py
+++ test3.py
@@ -5,7 +5,7 @@ from PyQt4 import QtGui, QtCore
 
 url='https://r1---sn-oxunxg8pjvn-bpbz7.googlevideo.com/videoplayback?mm=31&ip=189.123.8.217&mn=sn-oxunxg8pjvn-bpbz7&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Ckeepalive%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Crequiressl%2Csource%2Cexpire&mime=audio%2Fwebm&id=o-AFHMMPrV8d2SAGmfxcHi3rkzXigZoQ6D-I4Q_VvlvPT_&dur=4478.501&mt=1510956352&pl=22&mv=m&ms=au&requiressl=yes&expire=1510978063&initcwndbps=572500&itag=251&key=yt6&keepalive=yes&ei=rl0PWvb-NoPmwQS1-JG4Aw&source=youtube&signature=60A645EE9A46BD0E34AC0C2F2AA91115539753C0.9C99446A08C0AF7B358EE3A300CF2D3B9ADC71E6&clen=68389315&gir=yes&ipbits=0&lmt=1510151293556017&ratebypass=yes'
 urlv='https://r1---sn-oxunxg8pjvn-bpbz7.googlevideo.com/videoplayback?mm=31&ip=189.123.8.217&mn=sn-oxunxg8pjvn-bpbz7&sparams=aitags%2Cclen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Ckeepalive%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Crequiressl%2Csource%2Cexpire&mime=video%2Fwebm&id=o-AFHMMPrV8d2SAGmfxcHi3rkzXigZoQ6D-I4Q_VvlvPT_&dur=4478.500&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278%2C298%2C299%2C302%2C303&mt=1510956352&pl=22&mv=m&ms=au&requiressl=yes&expire=1510978063&initcwndbps=572500&itag=248&key=yt6&keepalive=yes&ei=rl0PWvb-NoPmwQS1-JG4Aw&source=youtube&signature=5E7CBD9930EB3B204625FD483D9DE65BC056C875.06318CFEDBFE4A0D493A679E6B6533B7182855D7&clen=1445171528&gir=yes&ipbits=0&lmt=1510151627800863&ratebypass=yes'
-
+position2=0
 class Player(QtGui.QMainWindow):
     """A simple Media Player using VLC and Qt
     """
@@ -18,6 +18,7 @@ class Player(QtGui.QMainWindow):
         # creating an empty vlc media player
         self.mediaplayer = self.instance.media_player_new()
         self.mediaplayer2 = self.instance.media_player_new()
+        self.events = self.mediaplayer.event_manager()
 
         self.createUI()
         self.isPaused = False
@@ -126,10 +127,8 @@ class Player(QtGui.QMainWindow):
         # put the media in the media player
         self.mediaplayer.set_media(self.media)
         self.mediaplayer2.set_media(self.media2)
-        events = self.mediaplayer.event_manager()
 
-        events.event_attach(vlc.EventType.MediaPlayerPlaying,
-                            self.open2)
+        self.events.event_attach(vlc.EventType.MediaPlayerPlaying, self.open2)
 
         # parse the metadata of the file
         self.media.parse()
@@ -157,11 +156,18 @@ class Player(QtGui.QMainWindow):
         """
         self.mediaplayer.audio_set_volume(Volume)
 
+    def change2(self,status):
+        position2=self.mediaplayer.get_position()
+        self.mediaplayer2.set_position(position2)
+
+        self.events.event_detach(vlc.EventType.MediaPlayerPositionChanged)
     def setPosition(self, position):
         """Set the position
         """
+        self.events.event_attach(vlc.EventType.MediaPlayerPositionChanged,self.change2)
         # setting the position to where the slider was dragged
         self.mediaplayer.set_position(position / 1000.0)
+
         # the vlc MediaPlayer needs a float value between 0 and 1, Qt
         # uses integer variables, so you need a factor; the higher the
         # factor, the more precise are the results
